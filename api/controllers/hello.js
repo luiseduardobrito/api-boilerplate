2ebd8e522f56207ff71bf299166f1818c45da373
Luís Eduardo Brito <luiseduardo14@gmail.com>
2014-03-14 00:05:24 -0300

    express server inicializando com mensagem do r2-d2

---
+++ api/controllers/hello.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	world: function(req, res) {
+
+		res.json({
+			hello: "world"
+		});
+	}
+}
---
+++ config/routes.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+	"prefix": "/api",
+
+	"get": {
+
+		"/": {
+
+			controller:"hello",
+			method: "world",
+
+			filters: [""]
+		},
+
+		"/hello": {
+
+			controller:"hello",
+			method: "world",
+
+			filters: [""]
+		}
+	}
+}
